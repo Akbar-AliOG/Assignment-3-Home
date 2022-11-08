af6deb901fa0aa3502334ef91fa73f95cbc1d42b
Akbar <makbarali1105@gmail.com>
2022-11-08 03:21:51 -0500

    Home Copy

--- app.js
+++ app.js
@@ -1,60 +1,101 @@
 const getData = async (url, params) => {
-    try {
-      return await axios.get(url, params);
-    } catch (error) {
-      console.log(error);
+  try {
+    return await axios.get(url, params);
+  } catch (error) {
+    console.log(error);
+  }
+};  
+
+const getMovies1 = async () => {
+  let datatag = document.getElementById("datatag");
+  datatag.innerHTML = "";
+  let selectedMovie = document.getElementById("mainselect");
+  console.log("selected movie: " + selectedMovie.value);
+  const movieData = await getData("https://api.themoviedb.org/3/search/movie", {
+    params: {
+      api_key: "8d992bf093d92b23f2529662f9291664",
+      query: selectedMovie.value,
     }
-  };
+  });
+
   
-  const getMovies1 = async () => {
-    let datatag = document.getElementById("datatag");
-    datatag.innerHTML = "";
-    let selectedMovie = document.getElementById("mainselect");
-    console.log("selected movie: " + selectedMovie.value);
-    const movieData = await getData("https://api.themoviedb.org/3/search/movie", {
+  console.log("size: " + movieData.data.results.length)
+  if (movieData.data.results.length < 1) {
+    return;
+  }
+
+  
+    let movie = movieData.data.results[0];
+    console.log("movie: " + movie)
+    const extraData = await getData(`https://api.themoviedb.org/3/movie/${movie.id}`, {
       params: {
         api_key: "8d992bf093d92b23f2529662f9291664",
-        query: selectedMovie.value,
+        append_to_response: "videos",
       }
     });
-  
+
+    // Making Variables
+    const title = document.createElement("h1")
+    const release_date = document.createElement("h2")
+    const website = document.createElement("h3")
+    const runtime = document.createElement("h2")
+    const budget = document.createElement("h3")
+    const revenue = document.createElement("h3")
+    const popularity = document.createElement("h3")
+    const vote_average = document.createElement("h3")
+    const vote_count = document.createElement("h3")
+    const tagline = document.createElement("h3")
+    const overview = document.createElement('h4');
+    const iframe = document.createElement('iframe'); 
+    const img = document.createElement('img');
+    const trailer = extraData.data.videos.results.filter((video) => video.type === "Trailer").at(0).key;
     
-    console.log("size: " + movieData.data.results.length)
-    if (movieData.data.results.length < 1) {
-      return;
-    }
+    
+    // Giving HTML TAGS
+    title.setAttribute('id','title')
+    release_date.setAttribute('id','release_date')
+    website.setAttribute('id','website')
+    runtime.setAttribute('id','runtime')
+    budget.setAttribute('id','budget')
+    revenue.setAttribute('id','revenue')
+    popularity.setAttribute('id','popularity')
+    vote_average.setAttribute('id','vote_average')
+    vote_count.setAttribute('id','vote_count')
+    tagline.setAttribute('id','tagline')
+    overview.setAttribute('id','overview')
+    iframe.setAttribute('id','trailer')
+    img.setAttribute('id','image') 
+    
 
     
-      let movie = movieData.data.results[0];
-      console.log("movie: " + movie)
-      const extraData = await getData(`https://api.themoviedb.org/3/movie/${movie.id}`, {
-        params: {
-          api_key: "8d992bf093d92b23f2529662f9291664",
-          append_to_response: "videos",
-        }
-      });
+    // Data Storage 
+    title.innerHTML = `${extraData.data.original_title}`
+    release_date.innerHTML = `Release Date: ${movie.release_date}`
+    website.innerHTML= `Website: ${extraData.data.homepage}`
+    runtime.innerHTML = `Runtime: ${extraData.data.runtime} minutes`
+    budget.innerHTML = ` Budget: $${extraData.data.budget}`
+    revenue.innerHTML = `Revenue: $${extraData.data.revenue}`
+    popularity.innerHTML = `Popularity: ${extraData.data.popularity}`
+    vote_average.innerHTML = `Vote Average: ${extraData.data.vote_average} stars`
+    vote_count.innerHTML = `Vote Count: ${extraData.data.vote_count} people`
+    tagline.innerHTML = `Tagline: "${extraData.data.tagline}"`
+    overview.innerHTML = `Overview: ${movie.overview}`;
+    iframe.src = `https://www.youtube.com/embed/${trailer}`
+    img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
   
-      
-      const p = document.createElement('p');
-      p.setAttribute('id','para1') 
-      const img = document.createElement('img');
-      const iframe = document.createElement('iframe');
-      const h1 = document.createElement("h1")
-      const h2 = document.createElement("h2")
-      const h3 = document.createElement("h3")
-      const webpage = document.createElement("a")
-      
-      h1.innerHTML = `${extraData.data.original_title}`
-      h2.innerHTML = `${movie.release_date}--${movie.vote_average}--${extraData.data.runtime}`
-      h3.innerHTML = `${extraData.data.budget}--${extraData.data.revenue}--${extraData.data.popularity}`
-      p.innerHTML = `${movie.overview}`;
-      img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-      
-      datatag.append(h1);
-      datatag.append(h2);
-      datatag.append(h3)
-      datatag.append(p);
-      datatag.append(img);
-      
-    
-  };
+    // Adding to the DOM
+    datatag.append(img);
+    datatag.append(title);
+    datatag.append(release_date);
+    datatag.append(overview);
+    datatag.append(website);
+    datatag.append(runtime);
+    datatag.append(budget);
+    datatag.append(revenue);
+    datatag.append(popularity);
+    datatag.append(vote_average);
+    datatag.append(vote_count);
+    datatag.append(tagline);
+    datatag.append(iframe);
+  
+};
